04bad0c9d825a74a613a803b402c8473832f4553
kof99athena <kof99athena@gmail.com>
2023-04-10 21:41:34 +0900

    Update 36 : 메신저 ing, 급여명세서 출력 및 저장 기능 구현중

--- app/src/main/java/com/athena/projectgroupwareapp/drawer/SalaryActivity.kt
+++ app/src/main/java/com/athena/projectgroupwareapp/drawer/SalaryActivity.kt
@@ -1,45 +1,96 @@
 package com.athena.projectgroupwareapp.drawer
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.Toast
 import com.athena.projectgroupwareapp.R
 import com.athena.projectgroupwareapp.databinding.ActivityApprovalBinding
 import com.athena.projectgroupwareapp.databinding.ActivitySalaryBinding
+import com.bumptech.glide.Glide
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class SalaryActivity : AppCompatActivity() {
 
     val binding : ActivitySalaryBinding by lazy { ActivitySalaryBinding.inflate(layoutInflater) }
 
+    //파이어베이스 관련된 프로퍼티
+    var firestore : FirebaseStorage = FirebaseStorage.getInstance()  //인스턴스만들기
+    var strRef : StorageReference = firestore.getReference() //storage를 참조한다
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+
         val til = binding.textInput  //TextInputLayout을 잡고있는녀석
         var s = til.editText?.text.toString() //TextInputLayout에서 잡고있는 녀석을 글씨로가져온다.
 
-
         var sort : Array<String> = resources.getStringArray(R.array.salary) //array에 등록된 결재종류들
-        
 
         val adapter = ArrayAdapter.createFromResource(this,R.array.salary,android.R.layout.simple_list_item_1)
         binding.salarySelect.setAdapter(adapter)
 
 
-
         binding.salarySelect.setOnItemClickListener { parent, view, position, id ->
             var s : String = binding.salarySelect.text.toString()
-            Log.i("tag",s)
+
+            var jan : String = "2023salary/999999/salary_jan.png"
+            var fev : String = "2023salary/999999/salary_fev.png"
+            var mar : String = "2023salary/999999/salary_mar.png"
 
             when{
+                s.equals("2023년 1월 급여명세서")->{
+                    strRef.child(jan).downloadUrl.addOnSuccessListener(object : OnSuccessListener<Uri>{
+                        override fun onSuccess(p0: Uri?) {
+                            Glide.with(this@SalaryActivity).load(p0).into(binding.ivSalary)
+                            //binding.saveSalary.
+                        }
+
+                    }).addOnFailureListener {
+                        Toast.makeText(this, "${it.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                s.equals("2023년 2월 급여명세서")->{
+                    strRef.child(fev).downloadUrl.addOnSuccessListener(object : OnSuccessListener<Uri>{
+                        override fun onSuccess(p0: Uri?) {
+                            Glide.with(this@SalaryActivity).load(p0).into(binding.ivSalary)
+                        }
+
+                    }).addOnFailureListener {
+                        Toast.makeText(this, "${it.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                s.equals("2023년 3월 급여명세서")->{
+                    strRef.child(mar).downloadUrl.addOnSuccessListener(object : OnSuccessListener<Uri>{
+                        override fun onSuccess(p0: Uri?) {
+                            Glide.with(this@SalaryActivity).load(p0).into(binding.ivSalary)
+                        }
+
+                    }).addOnFailureListener {
+                        Toast.makeText(this, "${it.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
                 s.equals("2022년 급여명세서")->{
-                    Snackbar.make(binding.framelayoutSalary,"22년 급여명세서는 관리자에게 문의하세요.", Snackbar.LENGTH_INDEFINITE).setAction("X", {}).show()
+                    Snackbar.make(binding.ivSalary,"22년 급여명세서는 관리자에게 문의하세요.", Snackbar.LENGTH_INDEFINITE).setAction("X", {}).show()
                 }
                 else -> false
             }
 
         }//onItemCLickListener
-    }
+
+
+    }//onCreate
+
+
+
 }
