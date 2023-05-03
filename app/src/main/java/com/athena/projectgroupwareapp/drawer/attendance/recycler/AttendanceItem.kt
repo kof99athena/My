cc094605e04e5bf7d821e3e8e85ad3dd225a11d0
kof99athena <kof99athena@gmail.com>
2023-05-03 23:33:27 +0900

    Update 53 :Calendar 완성

--- app/src/main/java/com/athena/projectgroupwareapp/drawer/attendance/recycler/AttendanceItem.kt
+++ app/src/main/java/com/athena/projectgroupwareapp/drawer/attendance/recycler/AttendanceItem.kt
@@ -6,3 +6,7 @@ data class AttendanceItem(
     var today : String,
     var timeIn : String,
     var timeOut : String)
+
+data class CalListItem (
+    var title : String,
+    var dateOfIssue : String)
